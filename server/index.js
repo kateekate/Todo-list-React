69f3fb62d9c3d345b817dbbbd2ba51fcf08ea69f
Katya <katya.mikhaylova.98@list.ru>
2023-01-16 09:36:05 +0300

    init: routers

--- server/index.js
+++ server/index.js
@@ -1,3 +1,21 @@
-const run = () => console.log('Server is ok')
+const express = require('express')
+const router = require('./routes')
+const mongoose = require('mongoose')
+require('dotenv').config()
 
-setInterval(run, 3000)
+const app = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 9000
+
+
+mongoose.connect(process.env.DB_CONNECT)
+	.then(()=> console.log('Database connected'))
+	.catch(err => console.log(err))
+
+app.use('/api', router)
+
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
